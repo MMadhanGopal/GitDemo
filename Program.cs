46293fac44b671c60b9f50aff3a3111dbee13cb3
Madhan Gopal <madhangopal9704@gmail.com>
2021-11-11 23:01:35 +0530

    new changes

--- Program.cs
+++ Program.cs
@@ -1,12 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace GitDemo7
+// 'override' modifier will overrides the base class implementation
+
+namespace Demo
 {
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public virtual void Print()
+        {
+            Console.WriteLine($"{FirstName} {LastName}");
+        }
+    }
+
+    class Developer: Employee
+    {
+        public override void Print() // overrides the base class implementation
+        {
+            Console.WriteLine($"{FirstName} {LastName} Developer");
+        }
+    }
+
+    class Tester:Employee
+    {
+        public override void Print() // overrides the base class implementation
+        {
+            Console.WriteLine($"{FirstName} {LastName} Tester");
+        }
+    }
+
+    class Manager:Employee
+    {
+        List<Employee> m_empList = new List<Employee>();
+
+        public void AddEmployee(Employee employee)
+        {
+            m_empList.Add(employee);
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine($"Manager {FirstName} {LastName}");
+
+            foreach (Employee employee in m_empList)
+                employee.Print();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Manager manager = new Manager() { FirstName = "Madhan", LastName = "Mandla"};
+
+            manager.AddEmployee(new Developer { FirstName = "Naveen", LastName = "Baratam" });
+            manager.AddEmployee(new Tester { FirstName = "Bharath", LastName = "Moppuri" });
+
+            manager.AddEmployee(new Developer { FirstName = "Narasimha", LastName = "K" });
+            manager.AddEmployee(new Tester { FirstName = "Sujana", LastName = "Mannam" });
+           
+            manager.Print();
         }
     }
 }
+
